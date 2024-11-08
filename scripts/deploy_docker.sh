ec4b48f2ff45e217331942ffb68c067504407707
dhamatvivek20 <117385213+dhamatvivek20@users.noreply.github.com>
2024-11-08 15:30:37 +0530

    Update deploy_docker.sh
    small comment

--- scripts/deploy_docker.sh
+++ scripts/deploy_docker.sh
@@ -3,5 +3,5 @@
 # Pull the Docker image
 docker pull dhamatvivek/demo-react-app
 
-# Run the Docker container with a specified name
+# Run the Docker container with a specified name..
 docker run -d --name demo-react-app -p 80:80 dhamatvivek/demo-react-app
